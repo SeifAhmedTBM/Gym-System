a6ca9eeb7998dacd20a6f168145fd6101adc3f7c
AhmedSabryH <as0162121@gmail.com>
2024-08-25 15:24:06 +0300

    fix (GS-18-Fix_Refresh_Data)

--- resources/views/livewire/loading-memberships.blade.php
+++ resources/views/livewire/loading-memberships.blade.php
@@ -1,6 +1,29 @@
 <div class="form-inline mx-5">
-    <button wire:click="refresh" class="btn btn-warning btn-sm" wire:loading.class="d-none"><i class="fa fa-refresh"></i> Refresh Data</button>
-    <div wire:loading>
+    <button type="button" id="refreshButton" class="btn btn-warning btn-sm">
+        <i class="fa fa-refresh"></i> Refresh Data
+    </button>
+    <div id="loadingSpinner" class="d-none">
         <i class="fa fa-refresh fa-spin"></i> Processing ...
     </div>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const refreshButton = document.getElementById('refreshButton');
+        const loadingSpinner = document.getElementById('loadingSpinner');
+
+        refreshButton.addEventListener('click', function () {
+            refreshButton.classList.add('d-none');
+            loadingSpinner.classList.remove('d-none');
+            setTimeout(function () {
+                const currentUrl = new URL(window.location.href);
+                const queryParams = currentUrl.searchParams.toString();
+                if (queryParams) {
+                    window.location.href = `${currentUrl.origin}${currentUrl.pathname}?${queryParams}`;
+                } else {
+                    window.location.href = `${currentUrl.origin}${currentUrl.pathname}`;
+                }
+            }, 100);
+        });
+    });
+</script>
