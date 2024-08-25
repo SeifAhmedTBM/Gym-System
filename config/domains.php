64284d7c3ebd7c850b64a327d6dcf5bcaa64b4b2
ahmedhamddyy7 <ahmed.hamdy@thebrandmakerz.com>
2024-08-25 14:44:20 +0300

    test

--- config/domains.php
+++ config/domains.php
@@ -397,6 +397,27 @@ return [
         'add_to_class_in_invoice'       => true,
         'transactions_date'             => true
     ], // For mini
+    "http://localhost" => [
+        "employees_schedule"            => true,
+        "timeline_schedule"             => true,
+        "due_payment"                   => true,
+        "sessions_count"                => true,
+        "national_id"                   => false,
+        "email"                         => false,
+        "vat"                           => false,
+        'profile_attendance'            => false,
+        'profile_attendance_dashboard'  => false,
+        'sales_by'                      => false,
+        'show_schedule_dashboard'       => false,
+        'member_code_enabled'           => false,
+        'is_reviewed_invoices'          => false,
+        'trainer_services_option'       => false,
+        'sports_option'                 => true,
+        'minor'                         => true,
+        'short_reports'                 => false,
+        'add_to_class_in_invoice'       => true,
+        'transactions_date'             => true
+    ],
     "https://minigym.dotapps.net" => [
         "employees_schedule"            => false,
         "timeline_schedule"             => false,
