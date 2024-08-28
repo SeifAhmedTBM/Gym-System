7682bd5fe6b263a8d1935d648bd2299997dead34
Seif Ahmed <seif.ahmed@thebrandmakerz.com>
2024-08-28 16:15:05 +0300

    ops(domains): enable timeline_schedule in zfitnesseg.com

--- config/domains.php
+++ config/domains.php
@@ -234,7 +234,7 @@ return [
     ],
     "https://zfitnesseg.com" => [
         "employees_schedule"            => false,
-        "timeline_schedule"             => false,
+        "timeline_schedule"             => true,
         "due_payment"                   => true,
         "sessions_count"                => false,
         "national_id"                   => false,
