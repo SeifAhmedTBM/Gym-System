5e87df2afb23cff707511501972edcdba5ba4f7c
muhammedfawzy00 <m.fawzy@thebrandmakerz.com>
2024-09-16 15:38:03 +0300

    feat (Expenses List) : fix export created_at error and filter error

--- app/Exports/ExpensesExport.php
+++ app/Exports/ExpensesExport.php
@@ -27,7 +27,8 @@ class ExpensesExport implements FromCollection,WithHeadings
                 'amount'                 => $expense->amount ?? '-',
                 'expenses_category_id'   => $expense->expenses_category->name ?? '-',
                 'created_by'             => $expense->created_by->name ?? '-',
-                'created_at'             => $expense->created_at,
+                'created_at'             => $expense->created_at->format('Y-m-d'),
+                
             ];
         });
 
