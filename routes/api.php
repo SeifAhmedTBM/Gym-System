e50fb46c2a6bab67a1e5342a69e32c7509abfaa2
muhammedfawzy00 <m.fawzy@thebrandmakerz.com>
2024-10-09 18:15:51 +0300

    feat (Take Manual Attendance for PT)

--- routes/api.php
+++ routes/api.php
@@ -234,5 +234,8 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin'], 
     
     Route::post('requestPrivateTrainer' , 'FreePtRequestsController@Request_free_pt');
     Route::post('available_free_pt' , 'FreePtRequestsController@free_pt');
+
+
+    Route::middleware('auth:sanctum')->post('takeManualAttend' ,'AttendanceAPIController@takeManualAttend');
 });
  
