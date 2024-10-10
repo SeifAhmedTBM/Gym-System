9c4583ab0cbc2208e3c672d3b16ee679f32aa246
muhammedfawzy00 <m.fawzy@thebrandmakerz.com>
2024-10-10 11:09:13 +0300

    fix (Saving user ID in free pt requests)

---
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Branch;
+use App\Models\Lead;
+use App\Models\User;
+class NotificationController extends Controller
+{
+    public function index(){
+        $branches = Branch::get();
+        return view('admin.Notifications.index' , compact('branches'));
+    }
+
+
+    public function sendNotification(Request $request){
+        if($request->branch == null){
+            $users = Lead::whereNotNull('branch_id')->get(); 
+        }
+        else{
+            $users = Lead::where('branch_id' , $request->branch)->get();
+        }
+        
+      
+        $users_array = []; 
+
+        foreach ($users as $lead) {
+            $user = User::find($lead->user_id); 
+            if ($user) { 
+                $users_array[] = $user; 
+            }
+        }
+        dd($users_array);
+    }
+}
