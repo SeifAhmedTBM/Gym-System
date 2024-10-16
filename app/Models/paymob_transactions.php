b5b3c9a31671602655a6904b60d6d611bbb2add7
muhammedfawzy00 <m.fawzy@thebrandmakerz.com>
2024-10-16 10:31:31 +0300

    Fix (Paymob Transactions) : fix all errors

--- app/Models/paymob_transactions.php
+++ app/Models/paymob_transactions.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class paymob_transactions extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id' ,
+        'membership_id',
+        'transaction_amount',
+        'transaction_id',
+        'orderId',
+        'transaction_createdAt',
+        'paymentMethodType',
+        'paymentMethodSubType',
+    ];
+
+
+    public function membership(){
+        return $this->belongsTo(Membership::class , 'membership_id' , 'id');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class , 'user_id' , 'id');
+    }
 }
